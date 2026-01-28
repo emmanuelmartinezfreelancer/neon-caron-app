1f25cba7d7b2479d449762749918b2762ec37efc
Emmanuel Martínez <emmanuelmartinez.freelancer@gmail.com>
2026-01-28 06:42:59 -0600

    Cleanup

--- Neon Caron/Controllers/HomeViewController.swift
+++ Neon Caron/Controllers/HomeViewController.swift
@@ -11,15 +11,39 @@ import UIKit
 class HomeViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var greetingLabel: UILabel!
+  @IBOutlet weak var createCollectionButton: UIButton!
+  
   let paintingCollections = PaintingCollections()
   var collectionName = ""
   
   override func viewDidLoad() {
     super.viewDidLoad()
     configureTableView()
+    setupHeaderUI()
     navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
     navigationController?.navigationBar.shadowImage = UIImage()
     navigationController?.navigationBar.isTranslucent = true
+  }
+  
+  private func setupHeaderUI() {
+    // Configurar el saludo (por ahora estático, en Fase 2 cambiará según login)
+    greetingLabel.text = "Hello, stranger"
+    
+    // El botón ya está configurado en el Storyboard, pero podemos personalizarlo aquí si es necesario
+    // createCollectionButton ya tiene el título "Create Collection +" desde el Storyboard
+  }
+  
+  @IBAction func createCollectionTapped(_ sender: UIButton) {
+    // Por ahora, mostrar una alerta temporal
+    // En fases posteriores, abriremos la pantalla de crear colección
+    let alert = UIAlertController(
+      title: "Create Collection",
+      message: "Esta funcionalidad estará disponible pronto!",
+      preferredStyle: .alert
+    )
+    alert.addAction(UIAlertAction(title: "OK", style: .default))
+    present(alert, animated: true)
   }
   
   private func configureTableView() {
